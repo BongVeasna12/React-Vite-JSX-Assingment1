6d5bfde0588230527f9612d7ab05ca2df1e2b393
DemoSystem <nheoumveasna12@gmail.com>
2024-03-06 23:08:10 +0700

    update  content

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import './App.css'
 import MainPage from './component/Layout/MainPage/MainPage';
 import NavbarPage from './component/Layout/Page/NavbarPage';
 import FooterPage from './component/Layout/Page/FooterPage';
-import RatingCard from './component/Layout/MainPage/RatingCard';
+import RatingCard from './component/Layout/Page/RatingCard';
 import TableProduct from './component/Layout/MainPage/TableProduct';
 import Message from "./component/Layout/MainPage/Message";
 
